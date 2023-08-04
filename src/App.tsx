4355f10762371b577916654c43b28cfdfd1442a3
cho1n <cjaewon0217@naver.com>
2023-08-04 13:06:11 +0900

    Design: Footer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/main';
 import Nav from './components/common/Nav';
-import footer from './components/common/Footer';
+import {Footer} from './components/common/Footer';
 import React from 'react';
 
 function App() {
@@ -10,10 +10,10 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Nav />
+          <Footer />
           <Routes>
             <Route path="/" element={<Main />} />
           </Routes>
-          <footer />
         </BrowserRouter>
       </div>
   );
