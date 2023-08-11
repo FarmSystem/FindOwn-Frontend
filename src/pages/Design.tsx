1d6b1f26e2ffed6ded7b042de4f4be2a0a8ae3ce
cho1n <cjaewon0217@naver.com>
2023-08-11 16:42:08 +0900

    Feat: 이미지 드래그 및 파일 선택하여 업로드 기능 구현

--- src/pages/Design.tsx
+++ src/pages/Design.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import { Grid } from "@mui/material";
 import { Sidebar } from "../components/Design/Sidebar";
+import { InputBox } from "../components/Design/InputBox";
 
 const Container = styled(Grid)`
   width: 100%;
@@ -14,10 +15,12 @@ const Container = styled(Grid)`
   padding: 50px 0;
 `;
 
+
 export const Design = () => {
   return (
   <Container>
     <Sidebar />
+    <InputBox />
   </Container>
   );
 };
