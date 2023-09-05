c0bf1f9c544960afbdb7ff8a9186d26a328a5336
cho1n <cjaewon0217@naver.com>
2023-09-05 10:20:51 +0900

    Feat: 상표권 이미지 파일 상위 컴포넌트로 전달

--- src/components/Trademark/section1.tsx
+++ src/components/Trademark/section1.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { Grid } from "@mui/material";
 import { InputBox } from "./InputBox";
 
 const Container = styled(Grid)`
-    width: 100vw;
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding-top: 50px;
-    @media (max-width: 1400px) {
-        padding-top: 0px;
-    }
-`
+  width: 100vw;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 50px;
+  @media (max-width: 1400px) {
+    padding-top: 0px;
+  }
+`;
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: #52c07e;
+  border-radius: 10px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+`;
 
-export const Section1 = () => {
-    return (
-        <Container>
-            <InputBox />
-        </Container>
-    )
-}
+export const Section1: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  // Define a function to handle image selection and set selectedImage
+  const handleImageSelect = (file: File | null) => {
+    setSelectedImage(file);
+  };
+
+  return (
+    <Container>
+      <InputBox onImageSelect={handleImageSelect} />
+      <Button>분석 시작</Button>
+    </Container>
+  );
+};
