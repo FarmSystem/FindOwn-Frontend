f882b2681d8afa16d3a93a9fda8689c81b689f7c
g2Min <jimin9320@gmail.com>
2023-09-12 23:39:08 +0900

    feat: 로그인 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
-import LoginPage from "./pages/LoginPage";
+// import LoginPage from "./pages/LoginPage";
+import { Login } from "./pages/Login";
 import Nav from "./components/common/Nav";
 import { Register } from "./pages/Register";
 import { Design } from "./pages/Design";
@@ -15,7 +16,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/design" element={<Design />} />
           <Route path="/trademark" element={<TradeMark />} />
