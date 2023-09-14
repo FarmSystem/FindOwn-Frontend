89cbddf06b07d8469ecde4193f5ddec2e81103e1
g2Min <jimin9320@gmail.com>
2023-09-15 08:50:32 +0900

    feat: 로그인 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,15 +8,52 @@ import {
   Additional,
   SignUp
 } from './style';
+import React, { useState, FormEvent } from 'react';
+
+interface ApiResponse {
+  code: number;
+}
+
 
 export const Login = () => {
+  const [Email, setEmail] = useState<string>("");
+  const [Password, setPassword] = useState<string>("");
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>("");
+
+  const onEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!Email) {
+      return alert("아이디를 입력해주세요");
+    } else if (!Password) {
+      return alert("비밀번호를 입력해주세요");
+    } else {
+      let body = {
+        email: Email,
+        password: Password,
+      };
+
+      console.log("Email", Email);
+      console.log("Password", Password);
+    }
+    setLoading(true);
+  };
+
   return(
     <Origin>
       <Div>
           <Text>로그인</Text>
-          <IdInputForm placeholder='아이디'/>
-          <PwdInputForm placeholder='비밀번호'/>
-          <SubmitButton>로그인</SubmitButton>
+          <IdInputForm onChange={onEmailHandler} placeholder='아이디'/>
+          <PwdInputForm onChange={onPasswordHandler} placeholder='비밀번호'/>
+          <SubmitButton onClick={()=>onSubmitHandler}>로그인</SubmitButton>
           <Additional>
             <SignUp>회원가입</SignUp>
           </Additional>
