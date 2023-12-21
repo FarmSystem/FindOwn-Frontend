cc3d02a4c4b7a47ed8cde8f87f673900a2ab8290
cho1n <cjaewon0217@naver.com>
2023-12-21 16:37:17 +0900

    Feat: 게시판 작성기능 구현

--- src/components/Community/BoardList.tsx
+++ src/components/Community/BoardList.tsx
@@ -27,87 +27,35 @@ const ButtonContainer = styled.div`
   z-index: 2;
 `;
 
-const Table = styled.table`
-  width: 100%;
-  text-align: center;
-  border-collapse: collapse;
-  border-spacing: 0;
-  border-bottom: 1px solid #e5e5e5;
-  margin: 30px;
-
-  th {
-    border-top: 1px solid #656565;
-    padding: 4px;
-  }
-
-  td {
-    border-top: 1px solid #e5e5e5;
-    padding: 8px;
-  }
-`;
-
-const Thead = styled.thead`
-  font-size: 16px;
-`;
-
 export const BoardList = () => {
-  //   const [boardList, setBoardList] = useState<String>([]);
+  const [boardList, setBoardList] = useState([]);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     apiClient
-      .get("/api/v2/users/community")
+      .get("/api/v2/users/community/post")
       .then((response) => {
-        // setBoardList(response.data);
+        setBoardList(response.data);
       })
-
       .catch(function (error) {
         console.log(error);
       });
   }, []);
+
   return (
     <Container>
       <TextContainer>
         {" "}
         <h1>게시판</h1>
       </TextContainer>
-      <Table>
-        <colgroup>
-          <col width="5%" />
-          <col width="56%" />
-          <col width="10%" />
-          <col width="10%" />
-          <col width="8%" />
-          <col width="8%" />
-          <col width="8%" />
-        </colgroup>
-
-        <Thead>
-          <tr>
-            <th>No</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>태그명</th>
-            <th>작성일</th>
-            <th>댓글수</th>
-            <th>조회수</th>
-          </tr>
-        </Thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td style={{ overflow: "hidden" }}>
-              asdsadadadadaddaasdasasdsasdadasda
-            </td>
-            <td>최재원</td>
-            <td>기타질문</td>
-            <td>2023-10-15</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div>
+        <ul>
+          {boardList.map((board: any) => (
+            <li key={board.postId}> {board.title}</li>
+          ))}
+        </ul>
+      </div>
       <ButtonContainer>
         <Button
           sx={{
