37d03723d27364f1073af3c7640fc2433c433d34
g2Min <jimin9320@gmail.com>
2024-01-13 15:22:42 +0900

    feat: 스토리지 구현

---
+++ src/components/Storage/style.ts
@@ -0,0 +1,120 @@
+import styled from "@emotion/styled";
+
+//scrap
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid black;
+`;
+
+export const ScrapHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin-top: 20px;
+  margin-left: 10px;
+  align-items: center;
+  // border: 1px solid black;
+`;
+
+export const ScrapForIcon = styled.img`
+  width: 24px;
+  height: 32px;
+`;
+
+export const Text = styled.div`
+  text-decoration: underline;
+  color: #959595;
+  font-size: 16px;
+`;
+
+export const ScrapTable = styled.div`
+  margin-top: 20px;
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Block = styled.div`
+  height: 150px;
+  width: 1060px;
+  border: 1px solid black;
+  position: relative;
+  // align-items: space-evenly;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Scrapped = styled.img`
+  position: absolute;
+  top: 15px;
+  left: 80px;
+  width: 24px;
+  height: 32px;
+`;
+
+export const TitleBlock = styled.div`
+  width: 745px;
+  height: 46px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-top: 30px;
+  // margin-left: 300px;
+  font-size: 20px;
+`;
+
+export const DetailBox = styled.div`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  gap: 10px;
+  padding-left: 30px;
+`;
+
+export const AddDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 60px;
+  height: 30px;
+  gap: 20px;
+`;
+
+export const AddText = styled.div`
+  color: #475569;
+`;
+
+export const ContentBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Tagcontainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 40px;
+  align-items: center;
+
+`;
+
+export const HashTag = styled.div`
+  width: 120px;
+  height: 40px;
+  background-color: #F1F5F9EB;
+  color: #64748B;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // text-align: center;
+`;
+
+export const Date = styled.div`
+  color: #64748B;
+`;
--- src/states/jotaiStates.ts
+++ src/states/jotaiStates.ts
@@ -7,3 +7,6 @@ export const modalAtom = atom<boolean> (false);
 
 //마이페이지 메뉴 관리
 export const menuAtom = atom<string>("/mypage");
+
+//스토리지 스크랩, 작성글 관리
+export const storageAtom = atom<string>("scrap");
