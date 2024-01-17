8967993daf62a752753fc6e478702dd791cb03e9
g2Min <jimin9320@gmail.com>
2024-01-18 00:06:17 +0900

    feat: jotai 상태관리

--- src/states/jotaiStates.ts
+++ src/states/jotaiStates.ts
@@ -5,8 +5,17 @@ export const selectedTagAtom = atom<string | null>(null);
 //비밀번호 모달창 관리
 export const modalAtom = atom<boolean> (false);
 
+//원본이미지 모달창 관리
+export const imgModalAtom = atom<boolean> (false);
+
+//결과 저장하기 모달창 관리
+export const submitModalAtom = atom<boolean> (false);
+
 //마이페이지 메뉴 관리
 export const menuAtom = atom<string>("/mypage");
 
 //스토리지 스크랩, 작성글 관리
 export const storageAtom = atom<string>("scrap");
+
+//similar Item index 관리
+export const similarAtom = atom<number>(0);
