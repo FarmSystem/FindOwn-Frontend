eee5d2d103ab7abf33f7ccd7539e576c2529f131
cho1n <cjaewon0217@naver.com>
2024-01-18 20:08:24 +0900

    Feat: Result Detail Loading 추가

--- src/pages/TradeMark/MarkDetail.tsx
+++ src/pages/TradeMark/MarkDetail.tsx
@@ -22,73 +22,7 @@ import {
 import { ImageModal } from "../DetailList/ImageModal";
 import { SubmitModal } from "./SubmitModal";
 import { apiClient } from "../../apis/apiClient";
-
-export const MarkDetail = () => {
-  const navigate = useNavigate();
-  const [detail, setDetail] = useAtom(detailAtom);
-  const [bright, setBright] = useState(true);
-  const [currentImg, setCurrentImg] = useAtom(imgModalAtom);
-  const [submitBtn, setSubmitBtn] = useAtom(submitModalAtom);
-  const toggleBright = () => {
-    setBright(!bright);
-  };
-
-  //원본 이미지보기 모달
-  const onClickToggleModal = useCallback(() => {
-    setCurrentImg(!detail?.input_image);
-  }, [detail?.input_image]);
-
-  //결과 저장하기 모달
-  const handleSubmitModal = useCallback(() => {
-    setSubmitBtn(!submitBtn);
-  }, [submitBtn]);
-
-  const getDetail = async () => {
-    try {
-      await apiClient.get(``).then((res) => {
-        setDetail(res.data);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getDetail();
-  }, []);
-
-  return (
-    <Container>
-      {currentImg && <ImageModal onClickToggleModal={onClickToggleModal} />}
-      {submitBtn && <SubmitModal onClickToggleModal={handleSubmitModal} />}
-      <Option>
-        <LightCare
-          onClick={toggleBright}
-          src={bright ? brightVersion : darkVersion}
-        />
-        <NavService onClick={() => navigate(`/trademark`)}>
-          상표 판단 바로가기
-        </NavService>
-      </Option>
-      <ListContainer>
-        <AdditionalContainer>
-          <OriginalImage onClick={onClickToggleModal}>
-            원본 이미지 보기
-          </OriginalImage>
-          <DetailBox />
-          <SimilarItem />
-        </AdditionalContainer>
-        <AlarmContainer>
-          <ContainerBox>
-            <AlarmText>침해도는 다음과 같이 구분됩니다.</AlarmText>
-            <AlarmImg src={alarm} />
-          </ContainerBox>
-          <SubmitBtn onClick={handleSubmitModal}>결과 저장하기</SubmitBtn>
-        </AlarmContainer>
-      </ListContainer>
-    </Container>
-  );
-};
+import spinner from "../../assets/images/Spinner_2.svg";
 
 const AdditionalContainer = styled.div`
   height: auto;
@@ -130,3 +64,97 @@ const SubmitBtn = styled.button`
   border-radius: 5px;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
+
+export const MarkDetail = () => {
+  const navigate = useNavigate();
+  const [detail, setDetail] = useAtom(detailAtom);
+  const [bright, setBright] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [currentImg, setCurrentImg] = useAtom(imgModalAtom);
+  const [submitBtn, setSubmitBtn] = useAtom(submitModalAtom);
+  const toggleBright = () => {
+    setBright(!bright);
+  };
+
+  //원본 이미지보기 모달
+  const onClickToggleModal = useCallback(() => {
+    setCurrentImg(!detail?.input_image);
+  }, [detail?.input_image]);
+
+  //결과 저장하기 모달
+  const handleSubmitModal = useCallback(() => {
+    setSubmitBtn(!submitBtn);
+  }, [submitBtn]);
+
+  const getDetail = async () => {
+    try {
+      await apiClient.get(``).then((res) => {
+        setDetail(res.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDetail();
+    const storage = localStorage.getItem("token");
+    if (!storage) {
+      navigate(`/`);
+      alert("로그인이 필요한 서비스입니다.");
+    } else {
+      apiClient.defaults.headers.common["Authorization"] = `Bearer ${storage}`;
+    }
+  }, []);
+
+  return loading ? (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <img src={spinner} alt="spinner" />
+      <div style={{ color: "gray", fontSize: "1.1rem" }}>
+        결과 값을 받아오는 중입니다. 잠시만 기다려주세요.
+      </div>
+    </div>
+  ) : (
+    <Container>
+      {currentImg && <ImageModal onClickToggleModal={onClickToggleModal} />}
+      {submitBtn && <SubmitModal onClickToggleModal={handleSubmitModal} />}
+
+      <Option>
+        <LightCare
+          onClick={toggleBright}
+          src={bright ? brightVersion : darkVersion}
+        />
+        <NavService onClick={() => navigate(`/trademark`)}>
+          상표 판단 바로가기
+        </NavService>
+      </Option>
+
+      <ListContainer>
+        <AdditionalContainer>
+          <OriginalImage onClick={onClickToggleModal}>
+            원본 이미지 보기
+          </OriginalImage>
+          <DetailBox />
+          <SimilarItem />
+        </AdditionalContainer>
+
+        <AlarmContainer>
+          <ContainerBox>
+            <AlarmText>침해도는 다음과 같이 구분됩니다.</AlarmText>
+            <AlarmImg src={alarm} />
+          </ContainerBox>
+          <SubmitBtn onClick={handleSubmitModal}>결과 저장하기</SubmitBtn>
+        </AlarmContainer>
+      </ListContainer>
+    </Container>
+  );
+};
